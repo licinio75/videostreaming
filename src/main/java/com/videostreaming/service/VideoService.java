e6c21f5a30e54e603c356bb39df5f76a4a9171f1
Licinio Gómez <licinio.gomez@juntadeandalucia.es>
2024-12-17 14:47:10 +0100

    kafka added

--- src/main/java/com/videostreaming/service/VideoService.java
+++ src/main/java/com/videostreaming/service/VideoService.java
@@ -4,6 +4,7 @@ import com.videostreaming.model.Video;
 import com.videostreaming.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -22,8 +23,14 @@ public class VideoService {
     @Autowired
     private VideoRepository videoRepository;
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     @Value("${minio.bucket-name}")
     private String bucketName;
+
+    @Value("${kafka.topic.name}")
+    private String kafkaTopic;
 
     public Video uploadVideo(String fileName, MultipartFile file) throws Exception {
         String uniqueFileName = UUID.randomUUID() + "_" + fileName;
@@ -42,7 +49,12 @@ public class VideoService {
         Video video = new Video();
         video.setName(fileName);
         video.setUrl(fileUrl);
+        Video savedVideo = videoRepository.save(video);
 
-        return videoRepository.save(video);
+        // Enviar mensaje a Kafka
+        String message = String.format("Video uploaded: {id: %s, name: %s, url: %s}", savedVideo.getId(), savedVideo.getName(), savedVideo.getUrl());
+        kafkaTemplate.send(kafkaTopic, message);
+
+        return savedVideo;
     }
 }
